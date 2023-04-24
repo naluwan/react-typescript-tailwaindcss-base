615adc0d994325c60ae894898a679bdbf2e3d172
naluwan <street.baller06@gmail.com>
2023-04-24 15:24:38 +0800

    feat: project init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='text-red-800 font-bold text-center'>react-typescript-tailwindcss-base</div>
+  );
+};
+
+export default App;
